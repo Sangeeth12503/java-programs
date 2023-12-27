9d9101a5b542988700c6d9a72d10e32fe0a0c747
sangeeth <112326567+Sangeeth12503@users.noreply.github.com>
2023-12-27 20:04:09 +0530

    Add files via upload

---
+++ remove middle node(2095).java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        if (prev != null) {
+            prev.next = slow.next;
+        } else {
+            head = slow.next;
+        }
+       return head;
+    }
+}
