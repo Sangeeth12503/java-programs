8316dd47028c6ffd8e1522bee26ee5b70b4fa120
sangeeth <112326567+Sangeeth12503@users.noreply.github.com>
2023-12-27 20:35:39 +0530

    Add files via upload

---
+++ prime number of set bits in their binary representation.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        int ans=0;
+        while(left<=right) {
+            int cnt=Integer.bitCount(left);
+            if(cnt==2 || cnt==3 || cnt==5 || cnt==7 || cnt==11 || cnt==13 || cnt==17 || cnt==19)
+                ans++;
+                left++;
+        }
+        return ans;
+    }
+}
